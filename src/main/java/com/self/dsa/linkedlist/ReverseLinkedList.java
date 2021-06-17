faadfd371c6927e0b9c8fe958d85cd27bec6c68f
sanjanarjn <sanjanarjn@gmail.com>
2021-06-17 12:22:12 +0530

    Reverse a linkedlist in O(1) space and O(n) complexity

---
+++ src/main/java/com/self/dsa/linkedlist/ReverseLinkedList.java
@@ -0,0 +1,34 @@
+package com.self.dsa.linkedlist;
+
+
+/**
+ * 
+ * Reverse a linkedlist in O(1) space and O(n) complexity
+ * @author sanjanarjn
+ *
+ */
+public class ReverseLinkedList {
+
+
+	public static ListNode reverseLinkedList(ListNode head) {
+		
+		
+		 if(head == null || head.next == null) {
+			 return head;
+		 }
+		 
+		 ListNode reversedListHead = head;
+		 ListNode reversedListTail = head;
+		 
+		 ListNode current = head.next;
+		 
+		 while(current != null) {
+			 reversedListTail.next = current.next;
+			 current.next = reversedListHead;
+			 
+			 reversedListHead = current;
+			 current = reversedListTail.next;
+		 }
+		 return reversedListHead;
+	}
+}
